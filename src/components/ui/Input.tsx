cdbaf7e2ad4e18b54fe58d4f3d22e80c167b68e6
fotedev <ahmedaboalayoun0016k@gmail.com>
2026-02-22 14:37:33 +0200

    fix: resolve empty interface and unnecessary dependency to fix Vercel build

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  // Add any extra props here if needed in the future
+  label?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
